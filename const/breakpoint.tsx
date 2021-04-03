5821875de84d5a4a42c6e0449b2f405b32cf1bb3
CLEE-youty <y.s.spector.bass@gmail.com>
2021-04-03 14:40:41 +0900

    init

---
+++ const/breakpoint.tsx
@@ -0,0 +1,6 @@
+export const maxHeaderWidth = 1200;
+export const commonHeader = 1024;
+export const commonFooter = 1024;
+export const wideContents = 960;
+export const contents = 768;
+export const tightContents = 699;
---
+++ const/color.tsx
@@ -0,0 +1,5 @@
+export const white = "#fff";
+export const brandColor = "#20aee5";
+export const borderColor = "#129bd1";
+export const textColor = "#555";
+export const footerColor = "#737b7f";
